66ab0be35c4e331cd5f38c5cff9e9460c7081bc9
Christopher Beltran <christod.beltran@gmail.com>
2018-07-02 19:03:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(*nums)
+  nums.each { |num|
+    puts "#{num} is #{ is_prime?(num) ? '' : "not " }a prime" }
+end
+
+def is_prime?(num)
+  (2...Math.sqrt(num)).all? { |i| num % i != 0 }
+end
